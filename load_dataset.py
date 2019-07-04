4b2705b403e20451ff58ac83956be562bb6d3da8
Nasim Rahaman <nasim.rahaman@tuebingen.mpg.de>
2019-07-04 14:44:27 +0200

    - even ʼmoar docs

--- load_dataset.py
+++ load_dataset.py
@@ -14,15 +14,33 @@ from disentanglement_lib.data.ground_truth.named_data import get_named_ground_tr
 
 
 def get_dataset_name():
+    """Reads the name of the dataset from the environment variable `AICROWD_DATASET_NAME`."""
     return os.getenv("AICROWD_DATASET_NAME", "cars3d")
 
 
 class DLIBDataset(Dataset):
     """
     No-bullshit data-loading from Disentanglement Library, but with a few sharp edges.
+
+    Sharp edge:
+        Unlike a traditional Pytorch dataset, indexing with _any_ index fetches a random batch.
+        What this means is dataset[0] != dataset[0]. Also, you'll need to specify the size
+        of the dataset, which defines the length of one training epoch.
+
+        This is done to ensure compatibility with disentanglement_lib.
     """
 
     def __init__(self, name, seed=0, iterator_len=50000):
+        """
+        Parameters
+        ----------
+        name : str
+            Name of the dataset use. You may use `get_dataset_name`.
+        seed : int
+            Random seed.
+        iterator_len : int
+            Length of the dataset. This defines the length of one training epoch.
+        """
         self.name = name
         self.seed = seed
         self.random_state = np.random.RandomState(seed)
@@ -43,12 +61,49 @@ class DLIBDataset(Dataset):
 
 
 def get_dataset(name=None, seed=0, iterator_len=50000):
+    """
+    Makes a dataset.
+
+    Parameters
+    ----------
+    name : str
+        Name of the dataset use. Defaults to the output of `get_dataset_name`.
+    seed : int
+        Random seed.
+    iterator_len : int
+        Length of the dataset. This defines the length of one training epoch.
+    Returns
+    -------
+    DLIBDataset
+    """
     name = get_dataset_name() if name is None else name
     return DLIBDataset(name, seed=seed, iterator_len=iterator_len)
 
 
 def get_loader(name=None, batch_size=32, seed=0, iterator_len=50000, num_workers=0,
                **dataloader_kwargs):
+    """
+    Makes a dataset and a data-loader.
+
+    Parameters
+    ----------
+    name : str
+        Name of the dataset use. Defaults to the output of `get_dataset_name`.
+    batch_size : int
+        Batch size.
+    seed : int
+        Random seed.
+    iterator_len : int
+        Length of the dataset. This defines the length of one training epoch.
+    num_workers : int
+        Number of processes to use for multiprocessed data-loading.
+    dataloader_kwargs : dict
+        Keyword arguments for the data-loader.
+
+    Returns
+    -------
+    DataLoader
+    """
     name = get_dataset_name() if name is None else name
     dlib_dataset = DLIBDataset(name, seed=seed, iterator_len=iterator_len)
     loader = DataLoader(dlib_dataset, batch_size=batch_size, shuffle=True,
