fe4c28f5b1bec9d0585d4e03d69077bdc6b3ed39
Nasim Rahaman <nasim.rahaman@tuebingen.mpg.de>
2019-07-04 10:50:58 +0200

    - simplified dataloading

--- load_dataset.py
+++ load_dataset.py
@@ -1,35 +1,24 @@
 import numpy as np
 import torch
 from torch.utils.data.dataset import Dataset
+from torch.utils.data.dataloader import DataLoader
 
 import os
-os.environ.update({'DISENTANGLEMENT_LIB_DATA': os.path.join(os.path.dirname(__file__),
-                                                            'scratch',
-                                                            'dataset')})
+if 'DISENTANGLEMENT_LIB_DATA' not in os.environ:
+    os.environ.update({'DISENTANGLEMENT_LIB_DATA': os.path.join(os.path.dirname(__file__),
+                                                                'scratch',
+                                                                'dataset')})
 
 # noinspection PyUnresolvedReferences
-from disentanglement_lib.data.ground_truth import cars3d, dsprites, mpi3d, norb, shapes3d
+from disentanglement_lib.data.ground_truth.named_data import get_named_ground_truth_data
 
 
 class DLIBDataset(Dataset):
     """
     No-bullshit data-loading from Disentanglement Library, but with a few sharp edges.
     """
-    # Registry of available datasets and the respective modules they reside in.
-
-    DATASETS = {
-        'Cars3D': 'cars3d',
-        'DSprites': 'dsprites',
-        'ColorDSprites': 'dsprites',
-        'NoisyDSprites': 'dsprites',
-        'ScreamDSprites': 'dsprites',
-        'MPI3D': 'mpi3d',
-        'SmallNORB': 'norb',
-        'Shapes3D': 'shapes3d'
-    }
 
     def __init__(self, name, seed=0, iterator_len=50000):
-        assert name in self.DATASETS, f"name must be one of: {list(self.DATASETS.keys())}"
         self.name = name
         self.seed = seed
         self.random_state = np.random.RandomState(seed)
@@ -37,9 +26,7 @@ class DLIBDataset(Dataset):
         self.dataset = self.load_dataset()
 
     def load_dataset(self):
-        dataset_module = globals().get(self.DATASETS[self.name])
-        assert dataset_module is not None
-        return getattr(dataset_module, self.name)()
+        return get_named_ground_truth_data(self.name)
 
     def __len__(self):
         return self.iterator_len
@@ -51,13 +38,22 @@ class DLIBDataset(Dataset):
         return torch.from_numpy(np.moveaxis(output, 2, 0))
 
 
-if __name__ == '__main__':
-    dlib_dataset = DLIBDataset('Cars3D')
-    print(dlib_dataset[0].shape)
-    # ^ prints `torch.Size([3, 64, 64])`
-    from torch.utils.data.dataloader import DataLoader
-    loader = DataLoader(dlib_dataset, 32, True, num_workers=2)
-    for b in loader:
+def get_loader(name, batch_size=32, seed=0, iterator_len=50000, num_workers=0):
+    dlib_dataset = DLIBDataset(name, seed=seed, iterator_len=iterator_len)
+    loader = DataLoader(dlib_dataset, batch_size=batch_size, shuffle=True,
+                        num_workers=num_workers)
+    return loader
+
+
+def test_loader():
+    loader = get_loader('cars3d', num_workers=2)
+    for count, b in enumerate(loader):
         print(b.shape)
         # ^ prints `torch.Size([32, 3, 64, 64])` and means that multiprocessing works
-        break
+        if count > 5:
+            break
+    print("Success!")
+
+
+if __name__ == '__main__':
+    test_loader()
