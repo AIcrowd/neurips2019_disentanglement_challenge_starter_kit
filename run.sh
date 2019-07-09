9c86d36fc11a6090dc6269b01f810a6f9c583771
Nasim Rahaman <nasim.rahaman@tuebingen.mpg.de>
2019-07-09 16:28:57 +0200

    - moved tensorflow train script to its own directory - updated run.sh to include pytorch and numpy scripts

--- run.sh
+++ run.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
-python ./train.py
+# Comment and uncomment as required:
+# Tensorflow
+python ./tensorflow/train_tensorflow.py
+
+# Pytorch
+#python ./pytorch/train_pytorch.py
+
+# Numpy
+#python ./numpy/train_numpy.py
