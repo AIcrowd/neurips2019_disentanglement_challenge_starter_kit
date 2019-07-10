eb981984af55615a5f79a564c710868f08e5da19
Nasim Rahaman <nasim.rahaman@tuebingen.mpg.de>
2019-07-10 14:22:56 +0200

    - refactored train_environ.sh

--- train_environ.sh
+++ train_environ.sh
@@ -1,17 +1,16 @@
 #!/usr/bin/env bash
 
+# Set up training environment.
+# Feel free to change these as required:
+export AICROWD_EVALUATION_NAME=thelittlevaethatcould
+export AICROWD_DATASET_NAME=cars3d
+
+# Change these only if you know what you're doing:
 # Check if the root is set; if not use the location of this script as root
-if ![ -n "${NDC_ROOT+set}" ]; then
+if [ ! -n "${NDC_ROOT+set}" ]; then
   export NDC_ROOT="$( cd "$(dirname "$0")" ; pwd -P )"
 fi
 
 export PYTHONPATH=${PYTHONPATH}:${NDC_ROOT}
-
-# Set up training environment.
-# Feel free to change these as required:
-export AICROWD_EVALUATION_NAME=experiment_name
-export AICROWD_DATASET_NAME=cars3d
-
-# Change these only if you know what you're doing:
 export AICROWD_OUTPUT_PATH=${NDC_ROOT}/scratch/shared
 export DISENTANGLEMENT_LIB_DATA=${NDC_ROOT}/scratch/dataset
